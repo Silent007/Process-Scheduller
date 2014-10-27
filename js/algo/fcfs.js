93750569f5b3af7b43ac65541cbae55ac7e2a824
Zeeshan Hassan <zishon89us@hotmail.com>
2014-10-27 16:43:12 +0500

    encoeaencode function, dev loop, dev variable

--- js/algo/fcfs.js
+++ js/algo/fcfs.js
@@ -21,6 +21,7 @@ function fcfs() {
         , arrTArr = arrivalTime()
         , process = []
         , totalTime = 0
+        , totalTimeDevBranch = 0
         , fcfs = []
         , dispatcher = new Process();
 
@@ -34,6 +35,11 @@ function fcfs() {
         process[i].srvcT = srvcTArr[i];
         totalTime += Number(process[i].srvcT);
         process[i].arrT = arrTArr[i];
+
+    }
+
+    for (var i = 0; i < 5; i++) {
+        console.log('Print Dev Branch');
 
     }
 
@@ -54,3 +60,30 @@ function fcfs() {
         incx += fcfs[i].srvcT * 30;
     }
 }
+
+/**
+ * XXTEA: encodes array of unsigned 32-bit integers using 128-bit key.
+ *
+ * @param   {number[]} v - Data vector.
+ * @param   {number[]} k - Key.
+ * @returns {number[]} Encoded vector.
+ */
+Tea.encode = function(v, k) {
+    if (v.length < 2) v[1] = 0;  // algorithm doesn't work for n<2 so fudge by adding a null
+    var n = v.length;
+
+    var z = v[n-1], y = v[0], delta = 0x9E3779B9;
+    var mx, e, q = Math.floor(6 + 52/n), sum = 0;
+
+    while (q-- > 0) {  // 6 + 52/n operations gives between 6 & 32 mixes on each word
+        sum += delta;
+        e = sum>>>2 & 3;
+        for (var p = 0; p < n; p++) {
+            y = v[(p+1)%n];
+            mx = (z>>>5 ^ y<<2) + (y>>>3 ^ z<<4) ^ (sum^y) + (k[p&3 ^ e] ^ z);
+            z = v[p] += mx;
+        }
+    }
+
+    return v;
+};
